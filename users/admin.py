e7dc789d549a46ce438234ccdd285bfbe7567f21
JohnatanPalacios <39398211+JohnatanPalacios@users.noreply.github.com>
2020-11-02 11:29:22 -0500

    login incompleto

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,40 @@
 # Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
-#Models
+# Models
 from users.models import Customer
+from django.contrib.auth.models import User
 
 
 @admin.register(Customer)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user')
-    list_display_links = ()
-    list_editable = ()
+    list_display = ('pk', 'user', 'photo')
+    list_display_links = ('pk', 'dni', 'username')
     search_fields = ('user__email', 'user__first_name', 'user__last_name', 'user__username')
-    list_filter = ()
+
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+
+    model = Customer
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
