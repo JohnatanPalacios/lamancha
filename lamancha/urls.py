e6e87b8146e5ce9a8545a0b4a75f7ecaf9a7ded9
JohnatanPalacios <39398211+JohnatanPalacios@users.noreply.github.com>
2020-12-04 11:18:37 -0500

    prueba

--- lamancha/urls.py
+++ lamancha/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from lamancha.views import *
 
 urlpatterns = [
@@ -48,3 +48,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # linea nueva para statics
+urlpatterns += staticfiles_urlpatterns()
