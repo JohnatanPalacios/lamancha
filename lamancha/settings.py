1ee2a6c40d6b5c23ff34bca533ef2f2ab6d5915e
JohnatanPalacios <39398211+JohnatanPalacios@users.noreply.github.com>
2020-12-04 06:24:01 -0500

    deploy

--- lamancha/settings.py
+++ lamancha/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a0up!a@%+kw)$h(aizbqwqfqvc3av6pbx^74j)$12^=&7*$lad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'lamanchautp.herokuapp.com']
 
